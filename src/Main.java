0f04482bb81f713ad6bbf8d15816c59abbfcdf71
Fibner <61762770+Fibner@users.noreply.github.com>
2025-10-18 16:12:03 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        try{
+            ObjectInputStream we = new ObjectInputStream(new FileInputStream(".\\klienci.dat"));
+        }catch (Exception e){}
         Scanner scanner = new Scanner(System.in);
         System.out.println("Witaj w Systemie obsługi kina.");
-        System.out.println("Dostępne seanse:");
+        System.out.println("Wybierz co chcesz zrobić:");
         //Tu będie wypisana lista seansów
-        System.out.println("Wybierz seans na który chcesz iść: ");
+        System.out.println("1 - Przeglądaj seanse");
+        System.out.println("2 - Wyświetl listę klientów (Admin)");
         int answear = Integer.parseInt(scanner.nextLine());
     }
 }
