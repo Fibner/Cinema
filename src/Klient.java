56cb33facd105f9b3e6a7af3c3b536ff1ef47ad1
Fibner <61762770+Fibner@users.noreply.github.com>
2025-10-18 15:25:06 +0200

    Update Klient.java

--- src/Klient.java
+++ src/Klient.java
@@ -1,6 +1,6 @@
 //Klasa Klient (nazwisko, imię, mail, telefon, seans, miejsce/miejsca - kolekcja)
-
-public class Klient {
+import java.io.Serializable;
+public class Klient implements Serializable {
     String nazwisko;
     String imie;
     String email;
@@ -16,6 +16,11 @@ public class Klient {
         this.telefon = telefon;
         this.seans = seans;
         this.zarezerwowane_miejsce = zarezerwowane_miejsce;
+    }
+
+    @Override
+    public String toString() {
+        return "Klient [nazwisko=" + nazwisko + ", imie=" + imie + ", email=" + email + ", telefon=" + telefon + ", seans=" + seans + ", zarezerwowane_miejsce=" + zarezerwowane_miejsce + "]";
     }
 }
 
